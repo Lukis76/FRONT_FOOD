81b5bf786604100ec5c021c38d1bc95c89c2af38
lukis76 <kiu.manoguerra@gmail.com>
2022-11-23 13:57:19 -0300

    front food

--- src/style/global.js
+++ src/style/global.js
@@ -10,4 +10,48 @@ body {
   background-size: cover;
   background-image: url(${img});
 }
+* {
+  padding: 0;
+  margin: 0;
+  font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
+    'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+    sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  box-sizing: border-box;
+  border: 0;
+  border: none;
+  text-decoration: none;
+}
+.img-active {
+  background: #0072bb66;
+}
+button {
+  border: 0;
+}
+
+a {
+  text-decoration: none;
+  outline: none;
+}
+a:link {
+  color: #50514f;
+}
+a:visited {
+  color: #50514f;
+}
+
+.next_page,
+.next_page:hover {
+  opacity: 0.2;
+}
+.last_page,
+.last_page:hover {
+  opacity: 0.2;
+}
+
+button {
+  border: none;
+  text-decoration: none;
+}
 `
