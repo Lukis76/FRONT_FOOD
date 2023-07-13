b9e57ccab711859fad984b08d9313aa50f95361a
Lucas Ruben Manoguerra <kiu.manoguerra@gmail.com>
2023-07-12 23:31:04 -0300

    updated back

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const BACKEND_FOOD = 'backend-food.up.railway.app/'
-export const BACKEND_FOOD = 'http://localhost:3088/'
+export const BACKEND_FOOD = 'https://food-service-postgres.onrender.com/'
+// export const BACKEND_FOOD = 'http://localhost:3088/'
